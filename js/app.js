f0c0438d1cbf5904091ce3fddc7df450c0ec85bd
EletronSz <janetuker2@gmail.com>
2019-11-01 17:33:37 +0200

    multer and glances

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+// const Foo = { template: '<div>foo</div>' }
+// const Bar = { template: '<div>bar</div>' }
+
+// const routes = [
+//   { path: '/foo', component: Foo },
+//   { path: '/bar', component: Bar }
+// ]
+
+// const router = new VueRouter({
+//   routes // short for `routes: routes`
+// })
+
+
 var app = new Vue({
   el: '#app',
+  // router,
   data: {
     message: 'Hello Vue!',
     phone: "+268 7629 2565",
